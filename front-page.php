8d37577e77cea1f97ef38f5dbe18edda453982c2
HerAngelEyes <64784374+HerAngelEyes@users.noreply.github.com>
2024-10-17 17:24:32 +0000

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -5,7 +5,13 @@
     <p>This is your custom homepage content. You can add whatever you like here!</p>
     
     <!-- Add any content or features for the homepage -->
+Fonts
+==============================================*/
+@import url('https://fonts.googleapis.com/css2?family=Jost:wght@400;500;600;700;800&family=Roboto:wght@300;400;500;700&display=swap');
 
+/*==============================================
+
+    
 </div>
 
 <?php get_footer(); // Include the footer ?>
