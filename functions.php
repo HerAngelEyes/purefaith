0c39cb0cd63e821dc15b4e38e0fb5ec4433da547
HerAngelEyes <64784374+HerAngelEyes@users.noreply.github.com>
2024-10-17 17:44:33 +0000

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,3 +9,18 @@ function register_my_menus() {
     );
 }
 add_action('init', 'register_my_menus');
+function enqueue_slick() {
+    // Enqueue Slick Carousel CSS
+    wp_enqueue_style('slick-css', get_template_directory_uri() . '/path/to/slick.css');
+    wp_enqueue_style('slick-theme-css', get_template_directory_uri() . '/path/to/slick-theme.css');
+
+    // Enqueue jQuery (if not already included)
+    wp_enqueue_script('jquery');
+
+    // Enqueue Slick Carousel JS
+    wp_enqueue_script('slick-js', get_template_directory_uri() . '/path/to/slick.min.js', array('jquery'), null, true);
+    
+    // Custom script to initialize Slick Carousel
+    wp_enqueue_script('custom-slick-init', get_template_directory_uri() . '/path/to/custom-slick-init.js', array('slick-js'), null, true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_slick');
